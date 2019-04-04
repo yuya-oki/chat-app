1c3c6db87ce0af33d56e282264e1340c3758ccfb
yuya-oki <user@USER-no-MacBook-Air.local>
2019-04-04 14:54:16 +0900

    change file

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -4,5 +4,6 @@ class Api::MessagesController < ApplicationController
     respond_to do |format|
       format.html
       format.json {@new_message = Message.where('id > ?', params[:message][:id])}
+    end
   end
 end
