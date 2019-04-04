4687c7c9f40e138261631a039b2edb7bc1d7585e
yuya-oki <user@USER-no-MacBook-Air.local>
2019-04-04 15:31:31 +0900

    change files

--- app/controllers/api/messages_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::MessagesController < ApplicationController
-  def index
-    @messages = Message.all
-    respond_to do |format|
-      format.html
-      format.json {@new_message = Message.where('id > ?', params[:message][:id])}
-    end
-  end
-end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
     @messages = @group.messages.includes(:user)
       respond_to do |format|
         format.html
-        format.json
+        format.json{@new_message = Message.where('id > ?', params[:message][:id])}
       end
   end
 
